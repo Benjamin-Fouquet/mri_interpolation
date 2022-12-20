f71620324a1bea38c479fe4c48d510ae3b649f43
BigBoy <>
2022-12-20 11:10:27 +0100

    added L2 regularization to SirenNet

--- models.py
+++ models.py
@@ -169,7 +169,7 @@ class SirenNet(pl.LightningModule):
         return self(x)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=1e-5)
         return optimizer
 
     def set_parameters(self, theta):
