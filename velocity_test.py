b18cd49ac3203cb9b211a597621c38ea8d459927
BigBoy <fouquet.benjamin@live.fr>
2023-04-25 17:02:38 +0200

    Stable commit velocity, linked 22 april pres

--- velocity_test.py
+++ velocity_test.py
@@ -4,8 +4,18 @@ MLP and Siren ok. Be mindful of normalisation when usign one or the other
 Convergence happens
 end result coherent but far from needed quality
 Check if indices of predicitons match indices of verts for generation of form
-'''
 
+TEST ONE: f(idx, t) = (x, y, z)
+Test object:
+    deforming cube
+Tested Networks:
+    MLP
+    Siren
+Conclusion:
+    Convergence happens when using RMSE, end result not satisfactory
+Potential pitfalls
+    Indices of vertices may not be fixed, create issue when reconstructing volume. TODO: how to test this ?
+'''
 
 import trimesh
 import numpy as np
@@ -20,14 +30,15 @@ from dataclasses import dataclass
 import torch.nn.functional as F
 import torch.nn as nn
 import math
+import json
 
 @dataclass
 class BaseConfig:
     checkpoint_path = None #'lightning_logs/version_384/checkpoints/epoch=99-step=100.ckpt'
     image_path: str = 'data/cube.nii.gz'
     image_shape = nib.load(image_path).shape
-    batch_size: int = 245760 #int(np.prod(image_shape)) #int(np.prod(image_shape)) if len(image_shape) < 4 else 1 #743424 # 28 * 28  #21023600 for 3D mri #80860 for 2D mri#784 for MNIST #2500 for GPU mem ?
-    epochs: int = 500
+    batch_size: int = 10000 #int(np.prod(image_shape)) #int(np.prod(image_shape)) if len(image_shape) < 4 else 1 #743424 # 28 * 28  #21023600 for 3D mri #80860 for 2D mri#784 for MNIST #2500 for GPU mem ?
+    epochs: int = 50
     num_workers: int = os.cpu_count()
     device = [0] if torch.cuda.is_available() else []
     accumulate_grad_batches: MappingProxyType = None #MappingProxyType({200: 2}) #MappingProxyType({0: 5})
@@ -154,7 +165,6 @@ class SirenNet(pl.LightningModule):
         super().__init__()
         self.num_layers = num_layers
         self.dim_hidden = dim_hidden
-        self.losses = []
         self.lr = lr
 
         self.layers = nn.ModuleList([])
@@ -201,7 +211,6 @@ class SirenNet(pl.LightningModule):
         z = self(x)
 
         loss = torch.sqrt(F.mse_loss(z, y))
-        self.losses.append(loss.detach().cpu().numpy())
 
         self.log("train_loss", loss)
         return loss
@@ -224,77 +233,23 @@ class SirenNet(pl.LightningModule):
         self.load_state_dict(p_dict)
         self.eval()
         self.train()  # supposed to be important when you set parameters or load state
-                
-config = BaseConfig()
+        
+class SirenNetDelta(SirenNet):
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        z = self(x)
+        
+        pred = z + y
 
-mri_path = 'data/equinus_downsampled.nii.gz'
-mri_path = 'data/cube.nii.gz'
-stl_path = 'output.stl'
-label = np.zeros((nib.load(mri_path).shape))
+        loss = torch.sqrt(F.mse_loss(pred, y))
 
-cube = np.ones((128, 128, 128), dtype=np.float32)
-cube[:32, :, :] = 0
-cube[96:, :, :] = 0
+        self.log("train_loss", loss)
+        return loss
 
-cube[:, :32, :] = 0
-cube[:, 96:, :] = 0
-
-cube[:, :, :32] = 0
-cube[:, :, 96:] = 0
-
-nib.save(nib.Nifti1Image(cube, affine=np.eye(4)), 'data/cube.nii.gz')
-
-#alternative function using only trimesh
-def nii_2_mesh(mri_path, output_path='output.stl'):
-    data = nib.load(mri_path).get_fdata(dtype=np.float32)
-    #need masking ? Resampling to isotrope also
-    data = data / data.max()
-    mask = (data > 0.3) * 1.0
-    verts, faces, normals, values = measure.marching_cubes(mask, 0)
-    mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
-    mesh.show()
-    
-# nii_2_mesh(mri_path)
-    
-#deforme cube in mesh space
-data = nib.load(mri_path).get_fdata(dtype=np.float32)
-verts, faces, normals, values = measure.marching_cubes(data, 0)
-mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
-# mesh.show()
-
-t_range = 10
-cube_list = [mesh]
-for t in range(t_range):
-    verts[:len(verts) // 2] += 10 #create gross deformation
-    cube_list.append(trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals))
-    
-#deform (move?) cube in image space, are indices fixed ? LATER
-
-#create tensordataset. X is (indice, t), Y is (x, y, z)
-# indices = torch.linspace(start=0, end=len(verts), steps=len(verts)) #maybe you will have to 0, 1 it
-indices = torch.linspace(start=0, end=1, steps=len(verts)) #maybe you will have to 0, 1 it
-times = torch.FloatTensor([])
-for t in range(t_range):
-    times = torch.cat((times, torch.FloatTensor([t * 0.1]).repeat(len(indices))))
-
-indices = torch.tile(indices, dims=(10,))
-
-indices.unsqueeze(-1)
-times.unsqueeze(-1)
-X = torch.stack((indices, times), dim=-1)
-
-Y = torch.FloatTensor([])
-for i in range(t_range):
-    Y = torch.cat((Y, torch.FloatTensor(cube_list[i].vertices / 186))) #TODO better normalisation
-
-#F(idx, t) = (x, y, z)
-dataset = TensorDataset(X, Y)
-
-train_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True, num_workers=os.cpu_count())
-
-#Siren
-model = SirenNet(dim_in=config.dim_in, dim_hidden=config.dim_hidden, dim_out=config.dim_out, num_layers=config.num_layers, lr=config.lr)
-
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        return self(x) + y
+        
 #MLP
 class MLP(pl.LightningModule):
     def __init__(self, dim_in, dim_hidden, dim_out, num_layers, activation='ReLU', lr=0.0001) -> None:
@@ -333,25 +288,132 @@ class MLP(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=1e-5)
         return optimizer 
-            
-model = MLP(dim_in=config.dim_in, dim_hidden=config.dim_hidden, dim_out=config.dim_out, num_layers=config.num_layers, lr=config.lr)        
+    
+class MLPDelta(MLP):
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        z = self(x)
+        
+        pred = z + y
+
+        loss = torch.sqrt(F.mse_loss(pred, y))
+
+        self.log("train_loss", loss)
+        return loss
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        return self(x) + y
+          
+                       
+config = BaseConfig()
+with open("config/hash_config.json") as f:
+    enco_config = json.load(f)
+    
+model = MLP(dim_in=config.dim_in, dim_hidden=config.dim_hidden, dim_out=config.dim_out, num_layers=config.num_layers, lr=config.lr)              
+
+mri_path = 'data/equinus_downsampled.nii.gz'
+mri_path = 'data/cube.nii.gz'
+stl_path = 'output.stl'
+label = np.zeros((nib.load(mri_path).shape))
+
+cube = np.ones((128, 128, 128), dtype=np.float32)
+cube[:32, :, :] = 0
+cube[96:, :, :] = 0
+
+cube[:, :32, :] = 0
+cube[:, 96:, :] = 0
+
+cube[:, :, :32] = 0
+cube[:, :, 96:] = 0
+
+nib.save(nib.Nifti1Image(cube, affine=np.eye(4)), 'data/cube.nii.gz')
+
+#alternative function using only trimesh
+def nii_2_mesh(mri_path, output_path='output.stl'):
+    data = nib.load(mri_path).get_fdata(dtype=np.float32)
+    #need masking ? Resampling to isotrope also
+    data = data / data.max()
+    mask = (data > 0.3) * 1.0
+    verts, faces, normals, values = measure.marching_cubes(mask, 0)
+    mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
+    mesh.show()
+    
+# nii_2_mesh(mri_path)
+    
+#deforme cube in mesh space
+data = nib.load(mri_path).get_fdata(dtype=np.float32)
+verts, faces, normals, values = measure.marching_cubes(data, 0)
+mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
+# mesh.show()
+
+t_range = 10
+cube_list = []
+for t in range(t_range):
+    if t == 0:
+        cube_list.append(mesh)
+    else:
+        verts[:len(verts) // 2] += 10 #create gross deformation
+        cube_list.append(trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals))
+    
+#deform (move?) cube in image space, are indices fixed ? LATER
+
+#create tensordataset. X is (indice, t), Y is (x, y, z)
+# indices = torch.linspace(start=0, end=len(verts), steps=len(verts)) #maybe you will have to 0, 1 it
+indices = torch.linspace(start=-1 if isinstance(model, SirenNet) else 0, end=1, steps=len(verts)) #maybe you will have to 0, 1 it
+
+times = torch.FloatTensor([])
+for t in range(t_range):
+    times = torch.cat((times, torch.FloatTensor([t * 0.1]).repeat(len(indices))))
+
+indices = torch.tile(indices, dims=(10,))
+
+if config.dim_in == 4:
+    times = times.unsqueeze(-1) #remove if using indices
+
+coords = torch.FloatTensor([])
+for cube in cube_list:
+    coords = torch.cat((coords, torch.FloatTensor(cube.vertices)))
+    
+coords = (coords - coords.min()) / (coords.max() - coords.min())
+if isinstance(model, SirenNet):
+    coords = coords * 2 - 1
+    
+if config.dim_in == 2:
+    X = torch.stack((indices, times), dim=-1)
+else:
+    X = torch.cat((coords, times), dim=-1)
+
+Y = torch.FloatTensor([])
+for i in range(t_range):
+    # Y = torch.cat((Y, torch.FloatTensor(cube_list[i].vertices / 186))) 
+    Y = torch.cat((Y, torch.FloatTensor(cube_list[i].vertices))) 
+    
+Y = (Y - Y.min()) / (Y.max() - Y.min())
+if isinstance(model, SirenNet):
+    Y = Y * 2 - 1
+
+#F(idx, t) = (x, y, z)
+dataset = TensorDataset(X, Y)
+
+train_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True, num_workers=os.cpu_count() // 4)
+
+test_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=False, num_workers=os.cpu_count() // 4)  
                 
 trainer = pl.Trainer(
     gpus=config.device,
     max_epochs=config.epochs,
     accumulate_grad_batches=dict(config.accumulate_grad_batches) if config.accumulate_grad_batches else None,
-    precision=32,
+    precision=16,
     # callbacks=[pl.callbacks.StochasticWeightAveraging(swa_lrs=1e-2)]
 )
 # trainer = pl.Trainer(gpus=config.device, max_epochs=config.epochs)
 trainer.fit(model, train_loader)
 
 #create a prediction
-pred = torch.concat(trainer.predict(model, train_loader))
+pred = torch.concat(trainer.predict(model, test_loader))
 
-for i in range(10):
+for i in range(t_range):
     pred_i = pred[24576 * i:24576 * (i + 1)] #first cube
-
-    #create mesh
-    predmesh = trimesh.Trimesh(vertices=pred.detach().cpu().numpy(), faces=faces, vertex_normals=normals)
-    predmesh.show()
+    predmesh = trimesh.Trimesh(vertices=pred_i.detach().cpu().numpy(), faces=faces, vertex_normals=normals)
+    predmesh.export(model.logger.log_dir + '/' + f'pred{i}.stl')
