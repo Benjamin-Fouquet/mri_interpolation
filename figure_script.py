12264d136821a70236be70a1fcad0ccc23548802
Benjamin-Fouquet <fouquet.benjamin@live.fr>
2023-07-07 09:19:17 +0200

    update figure script

--- figure_script.py
+++ figure_script.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 import imageio
 
-log_number = 436
+log_number = 12
 log_path = f'/home/benjamin/results_repaper/version_{log_number}/' 
 
 im = nib.load(log_path + 'interpolation.nii.gz').get_fdata()
@@ -20,15 +20,19 @@ for j in range(5):
         data = im[..., (j * 6 + i)]
         axes[i][j].imshow(data.T, origin="lower", cmap="gray")
 
+#TODO: parse config and get encotype
+title = 'Interpolation Hash3D + t'
 
-fig.suptitle('Interpolation hash 3D + t aniso', fontsize=16)        
+fig.suptitle(title, fontsize=16)        
 plt.savefig(log_path + 'interpolation.png')
+plt.clf()
 
 
 filenames = []
 
 for idx in range(im.shape[-1]):
     plt.imshow(im[..., idx].T, origin="lower", cmap="gray")
+    fig.suptitle(title, fontsize=16)   
     filename = f'file_{idx}.png'
     plt.savefig(filename)
     plt.close()
