adc6b9df7c2d7640b54430afe546baa338dec5ac
BigBoy <fouquet.benjamin@live.fr>
2023-07-07 09:16:10 +0200

    update modulated Siren

--- implementation_paper.py
+++ implementation_paper.py
@@ -22,6 +22,7 @@ from torch.utils.tensorboard import SummaryWriter
 from math import pi
 import torch.utils.data
 import matplotlib.pyplot as plt
+from models import Modulator, ModulatedSirenNet
 
 torch.manual_seed(1337)
 
@@ -32,21 +33,23 @@ class BaseConfig:
     # image_path: str = '/mnt/Data/FetalAtlas/template_T2.nii.gz'
     image_path: str = '/mnt/Data/Equinus_BIDS_dataset/sourcedata/sub_E01/sub_E01_dynamic_MovieClear_active_run_12.nii.gz'
     image_shape = nib.load(image_path).shape
-    batch_size: int = 50000 #~max #int(np.prod(image_shape)) #int(np.prod(image_shape)) if len(image_shape) < 4 else 1 #743424 # 28 * 28  #21023600 for 3D mri #80860 for 2D mri#784 for MNIST #2500 for GPU mem ?
+    batch_size: int = 250000 #~max #int(np.prod(image_shape)) #int(np.prod(image_shape)) if len(image_shape) < 4 else 1 #743424 # 28 * 28  #21023600 for 3D mri #80860 for 2D mri#784 for MNIST #2500 for GPU mem ?
     epochs: int = 50
     num_workers: int = os.cpu_count()
     device = [0] if torch.cuda.is_available() else []
     accumulate_grad_batches: MappingProxyType = None 
     # Network parameters
-    encoder_type: str = 'rff' #   
-    n_frequencies: int = 32 if encoder_type == 'tcnn' else 704 #for classic, n_out = 2 * n_freq. For tcnn, n_out = 2 * n_freq * dim_in
-    sigma: float = 8.0
-    n_frequencies_t: int = 4 if encoder_type == 'tcnn' else 30
-    sigma_t: float = 2.2
+    encoder_type: str = 'rff' #  
+    w0: float = 16.25
+    n_frequencies: int = 1235 #for classic, n_out = 2 * n_freq. For tcnn, n_out = 2 * n_freq * dim_in
+    sigma: float = 11.0
+    w0_t: float = 26.5
+    n_frequencies_t: int = 30
+    sigma_t: float = 2.12
     dim_in: int = len(image_shape)
-    dim_hidden: int = 64 
+    dim_hidden: int = 163 
     dim_out: int = 1
-    num_layers: int = 4
+    num_layers: int = 9
     skip_connections: tuple = () #(5, 11,)
     lr: float = 1e-3  # G requires training with a custom lr, usually lr * 0.1 
     interp_factor: int = 2
@@ -139,48 +142,6 @@ class Siren(torch.nn.Module):
         out = self.activation(out)
         return out
 
-
-class GaussianFourierFeatureTransform(torch.nn.Module):
-    """
-    An implementation of Gaussian Fourier feature mapping.
-
-    "Fourier Features Let Networks Learn High Frequency Functions in Low Dimensional Domains":
-       https://arxiv.org/abs/2006.10739
-       https://people.eecs.berkeley.edu/~bmild/fourfeat/index.html
-
-    Given an input of size [batches, num_input_channels, width, height],
-     returns a tensor of size [batches, mapping_size*2, width, height].
-    """
-
-    def __init__(self, num_input_channels, mapping_size=256, scale=10):
-        super().__init__()
-
-        self._num_input_channels = num_input_channels
-        self._mapping_size = mapping_size
-        self._B = torch.randn((num_input_channels, mapping_size)) * scale
-
-    def forward(self, x):
-        assert x.dim() == 4, 'Expected 4D input (got {}D input)'.format(x.dim())
-
-        batches, channels, width, height = x.shape
-
-        assert channels == self._num_input_channels,\
-            "Expected input to have {} channels (got {} channels)".format(self._num_input_channels, channels)
-
-        # Make shape compatible for matmul with _B.
-        # From [B, C, W, H] to [(B*W*H), C].
-        x = x.permute(0, 2, 3, 1).reshape(batches * width * height, channels)
-
-        x = x @ self._B.to(x.device)
-
-        # From [(B*W*H), C] to [B, W, H, C]
-        x = x.view(batches, width, height, self._mapping_size)
-        # From [B, W, H, C] to [B, C, W, H]
-        x = x.permute(0, 3, 1, 2)
-
-        x = 2 * pi * x
-        return torch.cat([torch.sin(x), torch.cos(x)], dim=1)
-
 #freq encoding tiny cuda
 class FreqMLP(pl.LightningModule):
     '''
@@ -194,8 +155,10 @@ class FreqMLP(pl.LightningModule):
         n_layers,
         skip_connections,
         encoder_type,
+        w0,
         n_frequencies,
         sigma,
+        w0_t,
         n_frequencies_t,
         sigma_t,
         lr,
@@ -209,8 +172,10 @@ class FreqMLP(pl.LightningModule):
         self.n_layers = n_layers
         self.skip_connections = skip_connections #index of skip connections, starting from 0
         self.lr = lr
+        self.w0 = w0
         self.n_frequencies = n_frequencies
         self.sigma = sigma
+        self.w0_t = w0_t
         self.n_frequencies_t = n_frequencies_t
         self.sigma_t = sigma_t
         self.encoder_type = encoder_type
@@ -218,8 +183,13 @@ class FreqMLP(pl.LightningModule):
 
         # self.encoder = tcnn.Encoding(n_input_dims=dim_in, encoding_config=config['encoding'])
         if self.encoder_type == 'siren':
-            self.encoder = Siren(dim_in=(self.dim_in - 1),dim_out=self.n_frequencies, is_first=True, w0=50.0, c=self.sigma)
-            self.encoder_t = Siren(dim_in=1 ,dim_out=self.n_frequencies_t, is_first=True, w0=75.0, c=self.sigma_t)
+            self.encoder = torch.nn.Sequential(Siren(dim_in=(self.dim_in - 1),dim_out=self.n_frequencies, is_first=True, w0=self.w0, c=self.sigma), Siren(dim_in=self.n_frequencies ,dim_out=self.n_frequencies, is_first=False, w0=self.w0, c=self.sigma))
+            self.encoder_t = Siren(dim_in=1 ,dim_out=self.n_frequencies_t, is_first=True, w0=self.w0_t, c=self.sigma_t)
+            self.encoding_dim_out = self.n_frequencies + self.n_frequencies_t
+            
+        elif self.encoder_type == 'modulated_siren':
+            self.encoder = ModulatedSirenNet(dim_in=(self.dim_in - 1), dim_hidden=self.n_frequencies, dim_out=self.n_frequencies, num_layers=2, w0_initial=self.w0, w0=self.w0, c=self.sigma)
+            self.encoder_t = Siren(dim_in=1 ,dim_out=self.n_frequencies_t, is_first=True, w0=self.w0_t, c=self.sigma_t)
             self.encoding_dim_out = self.n_frequencies + self.n_frequencies_t
             
         elif self.encoder_type == 'tcnn': #if tcnn is especially required, set it TODO: getattr more elegant
@@ -265,13 +235,8 @@ class FreqMLP(pl.LightningModule):
     def forward(self, x):
         coords = x[:, :(self.dim_in - 1)]
         t = x[:, -1].unsqueeze(-1)
-        if isinstance(self.encoder, GaussianFourierFeatureTransform):
-            coords = coords.unsqueeze(-1).unsqueeze(-1)
-            t = t.unsqueeze(-1).unsqueeze(-1)
         x = torch.hstack((self.encoder(coords), self.encoder_t(t)))
         skip = x.clone()
-        if isinstance(self.encoder, GaussianFourierFeatureTransform):
-            x = x.squeeze(-1).squeeze(-1)
         for idx, layer in enumerate(self.decoder):
             if idx in self.skip_connections:
                 x = torch.hstack((skip, x))
@@ -429,8 +394,10 @@ if config.checkpoint_path is not None:
                     dim_out=config.dim_out, 
                     n_layers=config.num_layers, 
                     skip_connections=config.skip_connections,
+                    w0=config.w0,
                     n_frequencies=config.n_frequencies,
                     sigma=config.sigma,
+                    w0_t=config.w0_t,
                     n_frequencies_t=config.n_frequencies_t,
                     sigma_t=config.sigma_t,                
                     encoder_type=config.encoder_type,
@@ -442,8 +409,10 @@ else:
                     dim_out=config.dim_out, 
                     n_layers=config.num_layers, 
                     skip_connections=config.skip_connections,
+                    w0=config.w0,
                     n_frequencies=config.n_frequencies,
                     sigma=config.sigma,
+                    w0_t=config.w0_t,
                     n_frequencies_t=config.n_frequencies_t,
                     sigma_t=config.sigma_t,                
                     encoder_type=config.encoder_type,
